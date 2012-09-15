582358a99dd47788af40ed3302e4323ed1432c3a
Mohamed Ragab Dahab <m.ragab@nilecode.com>
2012-09-15 22:22:39 +0200

    intial commit

---
+++ fuel/app/logs/2012/09/15.php
@@ -0,0 +1,6 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Warning - 2012-09-15 20:13:33 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-09-15 20:17:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-09-15 20:17:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-09-15 20:17:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
