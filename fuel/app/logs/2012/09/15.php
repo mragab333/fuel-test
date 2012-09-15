22ae1433b5531bfebb27ee0256ad7a9d090c41df
Mohamed Ragab <m_ragab333@yahoo.com>
2012-09-15 22:44:49 +0200

    creat scaffold message

--- fuel/app/logs/2012/09/15.php
+++ fuel/app/logs/2012/09/15.php
@@ -4,3 +4,4 @@ Warning - 2012-09-15 20:13:33 --> Fuel\Core\Fuel::init - The configured locale e
 Warning - 2012-09-15 20:17:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
 Warning - 2012-09-15 20:17:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
 Warning - 2012-09-15 20:17:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-09-15 20:44:25 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
